e939a31bf960edc57b968bbd94ee3bef3a4c67f8
kyzgxiaoguo <826189539@qq.com>
2019-12-16 22:06:03 +0800

    1.0.5

---
+++ hg_videoplayer/src/main/java/com/hg/hg_videoplayer/Test.java
@@ -0,0 +1,13 @@
+package com.hg.hg_videoplayer;
+
+/**
+ * @author Zhangzhenguo
+ * @create 2019/12/16
+ * @Email 18311371235@163.com
+ * @Describe
+ */
+public class Test {
+    public String myTest(){
+        return "ok";
+    }
+}
