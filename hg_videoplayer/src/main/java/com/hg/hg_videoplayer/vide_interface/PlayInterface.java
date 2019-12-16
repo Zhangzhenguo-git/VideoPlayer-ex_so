62d69a228696d7dd07b8fc28a9acebd3dfc14987
kyzgxiaoguo <826189539@qq.com>
2019-12-16 21:26:36 +0800

    Initial commit

---
+++ hg_videoplayer/src/main/java/com/hg/hg_videoplayer/vide_interface/PlayInterface.java
@@ -0,0 +1,38 @@
+package com.hg.hg_videoplayer.vide_interface;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * lrs
+ */
+public interface PlayInterface {
+       //开始播放
+//       void onVideoStart();
+       //开始加载
+       void onStartPrepared();
+       //加载完成
+       void onPrepared();
+       //播放暂停
+       void onVideoPause();
+       //继续播放
+       void onVideoResume();
+       //播放停止
+//       void onVideoStop();
+       //播放完成
+       void onVideoCompletion();
+       //播放进度
+       /**
+        * @param progress 当前播放进度（暂停后再播放可能会有跳动）
+        * @param secProgress 当前内存缓冲进度（可能会有0值）
+        * @param currentPosition 当前播放位置（暂停后再播放可能会有跳动）
+        * @param duration 总时长
+        */
+       void onVideoPlayListener(int progress, int secProgress, int currentPosition, int duration);
+       //播放出现问题
+       void onVideoError(int what, int extra);
+       //视图点击传递
+       void ViewonClick(View view);
+       //视图 滑动传递
+       void ViewonTouch(View v, MotionEvent event);
+}
